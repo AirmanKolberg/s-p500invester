7760f92136f8da965afe5c45e3a62c7a0b7afddd
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-12-22 22:51:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,108 +1,161 @@
 import pandas as pd
 import requests
 import math
-from secrets import real_token
-
-stocks = pd.read_csv('sp_500_stocks.csv')
-symbol = 'AAPL'
-
-real_api_url = f'https://cloud.iexapis.com/stable/stock/{symbol}/quote/?token={real_token}'
-data = requests.get(real_api_url).json()
-
-price = data['latestPrice']
-market_cap = data['marketCap']
-
-my_columns = ['Ticker', 'Stock Price', 'Market Capitalisation', 'Number of Shares to Buy']
-final_dataframe = pd.DataFrame(columns=my_columns)
-
-final_dataframe = final_dataframe.append(
-    pd.Series(
-        [
-            symbol,
-            price,
-            market_cap,
-            'N/A'
-        ],
-        index=my_columns
-    ),
-    ignore_index=True
-)
-
-# print('Take 1:\n')
-# print(final_dataframe)
+from time import sleep
+from datetime import date, datetime
+from os import system
+import calendar
+from secrets import api_token, stocks_dictionary, stocks_list
 
 
-def chunks(lst, n):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+def bash_command(user_in):
+    _ = system(user_in)
 
 
-symbol_groups = list(chunks(stocks['Ticker'], 100))
-symbol_strings = []
-for i in range(0, len(symbol_groups)):
-    symbol_strings.append(','.join(symbol_groups[i]))
+def clear_screen():
+    _ = system('clear')
 
-final_dataframe = pd.DataFrame(columns=my_columns)
 
-for symbol_string in symbol_strings:
-    batch_api_call_url = f"https://cloud.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote&token={real_token}"
-    data = requests.get(batch_api_call_url).json()
-    for symbol in symbol_string.split(','):
-        final_dataframe = final_dataframe.append(
-            pd.Series([symbol,
-                       data[symbol]['quote']['latestPrice'],
-                       data[symbol]['quote']['marketCap'],
-                       'N/A'],
-                      index=my_columns),
-            ignore_index=True)
+def check_prices(stock_or_all, price_or_shares2buy):
+    if price_or_shares2buy.lower() == 'price':
+        price_or_shares2buy = 'Stock Price'
+    elif price_or_shares2buy.lower() == 'shares2buy':
+        price_or_shares2buy = 'Shares to Buy'
+    else:
+        print(f"{price_or_shares2buy} is neither 'prices', nor 'shares2buy', sorry.")
+        return
+    stocks = pd.read_csv('stocks_list.csv')
+    my_columns = ['Ticker', 'Stock Price', 'Shares to Buy']
 
-# print(final_dataframe)
+    def chunks(lst, n):
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
 
-portfolio_size = input('Enter the value of your portfolio: ')
-try:
-    val = float(portfolio_size)
-except ValueError:
-    portfolio_size = input("That's not a number, please try again.\nEnter the value of your portfolio: ")
-    val = float(portfolio_size)
+    symbol_groups = list(chunks(stocks['Ticker'], 100))
+    symbol_strings = []
+    for i in range(0, len(symbol_groups)):
+        symbol_strings.append(','.join(symbol_groups[i]))
 
-position_size = float(portfolio_size) / len(final_dataframe.index)
-for i in range(0, len(final_dataframe['Ticker'])-1):
-    final_dataframe.loc[i, 'Number Of Shares to Buy'] = math.floor(position_size / final_dataframe['Stock Price'][i])
-print(final_dataframe)
+    final_dataframe = pd.DataFrame(columns=my_columns)
 
-writer = pd.ExcelWriter('recommended_trades.xlsx', engine='xlsxwriter')
-final_dataframe.to_excel(writer, 'Recommended Trades', index=False)
-background_colour = '#54bb99'
-font_colour = '#ffffff'
+    for symbol_string in symbol_strings:
+        batch_api_call_url = f"https://cloud.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote&token={api_token}"
+        data = requests.get(batch_api_call_url).json()
+        for symbol in symbol_string.split(','):
+            final_dataframe = final_dataframe.append(
+                pd.Series([symbol,
+                           data[symbol]['quote']['latestPrice'],
+                           'N/A'],
+                          index=my_columns),
+                ignore_index=True)
 
-string_format = writer.book.add_format(
-    {
-        'font_color': font_colour,
-        'bg_color': background_colour,
-        'border': 1
-    }
-)
+    portfolio_size = 6000
 
-dollar_format = writer.book.add_format(
-    {
-        'num_format': '$0.00',
-        'font_color': font_colour,
-        'bg_color': background_colour,
-        'border': 1
-    }
-)
+    # The following lines are unnecessary unless the user inputs portfolio_size
 
-integer_format = writer.book.add_format(
-    {
-        'num_format': '0',
-        'font_color': font_colour,
-        'bg_color': background_colour,
-        'border': 1
-    }
-)
+    # try:
+    #     val = float(portfolio_size)
+    # except ValueError:
+    #     portfolio_size = input("That's not a number, please try again.\nEnter the value of your portfolio: ")
+    #     val = float(portfolio_size)
 
-writer.sheets['Recommended Trades'].set_column('A:A', 18, string_format)
-writer.sheets['Recommended Trades'].set_column('B:B', 18, string_format)
-writer.sheets['Recommended Trades'].set_column('C:C', 18, string_format)
-writer.sheets['Recommended Trades'].set_column('D:D', 18, string_format)
-writer.save()
+    position_size = float(portfolio_size) / len(final_dataframe.index)
+    for i in range(0, len(final_dataframe['Ticker'])):
+        final_dataframe.loc[i, 'Shares to Buy'] = math.floor(position_size / final_dataframe['Stock Price'][i])
+
+    if stock_or_all.lower() == 'all':
+        return final_dataframe
+    else:
+        return final_dataframe.iloc[stocks_dictionary[f'{stock_or_all}']][f'{price_or_shares2buy}']
+
+
+def get_date():
+    current_date = date.today().strftime("%d %B %Y")
+    return current_date
+
+
+def get_time():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return current_time
+
+
+def get_weekday():
+    day_of_the_week = calendar.day_name[date.today().weekday()]
+    return day_of_the_week
+
+
+def get_hour():
+    if int(get_time()[0]) == 0:
+        hour = int(get_time()[1])
+    else:
+        hour = int(get_time()[0:2])
+    return hour
+
+
+# Invert both booleans when not debugging
+weekend = False
+market_open = True
+
+# --------------------TERMINAL BEGINS HERE--------------------
+
+message = f"Starting with $6,000 on {get_date()} at {get_time()}"
+print(message)
+
+while weekend:
+    if get_weekday() == 'Sunday' or get_weekday() == 'Saturday':
+        print("It's the weekend!  The market is closed.")
+        sleep(3600)
+    else:
+        weekend = False
+
+# if get_hour() >= 15:
+#     sleep((24 - get_hour()) * 3600)
+#
+# # Sleep until the time is HH:MM:00
+# sleep(60 - int(get_time()[-2:]))
+#
+# # Sleep until the time is HH:00:00
+# sleep((60 - int(get_time()[3:5])) * 60)
+#
+# hours_remaining = 7 - get_hour()
+#
+# # Sleep until the time is 09:00:00 EST
+# sleep(hours_remaining * 3600)
+
+while not market_open:
+    if get_time() == '07:00:00':
+        market_open = True
+    elif get_time() == '06:59:59':
+        sleep(1)
+        market_open = True
+    else:
+        print(f'The time is {get_time()}')
+        sleep(1)
+        clear_screen()
+
+initial_check = check_prices('all', 'price')
+tsla_stocks = initial_check['Shares to Buy'][0]
+rtx_stocks = initial_check['Shares to Buy'][1]
+hon_stocks = initial_check['Shares to Buy'][2]
+kbr_stocks = initial_check['Shares to Buy'][3]
+aapl_stocks = initial_check['Shares to Buy'][4]
+
+tsla_price = initial_check['Stock Price'][0]
+rtx_price = initial_check['Stock Price'][1]
+hon_price = initial_check['Stock Price'][2]
+kbr_price = initial_check['Stock Price'][3]
+aapl_price = initial_check['Stock Price'][4]
+
+tsla_value = tsla_price * tsla_stocks
+rtx_value = rtx_price * rtx_stocks
+hon_value = hon_price * hon_stocks
+kbr_value = kbr_price * kbr_stocks
+aapl_value = aapl_price * aapl_stocks
+total_value = tsla_value + rtx_value + hon_value + kbr_value + aapl_value
+
+print(f"Buying {tsla_stocks} TSLA @ {tsla_price} (Total: {tsla_value})")
+print(f"Buying {rtx_stocks} TSLA @ {rtx_price} (Total: {rtx_value})")
+print(f"Buying {hon_stocks} TSLA @ {hon_price} (Total: {hon_value})")
+print(f"Buying {kbr_stocks} TSLA @ {kbr_price} (Total: {kbr_value})")
+print(f"Buying {aapl_stocks} TSLA @ {aapl_price} (Total: {aapl_value})")
+print(f"Total spent: {total_value}/6000 ({(6000 - total_value).__round__(2)} remaining)")
