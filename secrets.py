b5a5ee5cf43c7d9bcf2ac077ea900d0e52d87d78
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-12-27 20:41:14 -0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,15 +1,1 @@
 api_token = 'pk_AndOtherDigitsHere'
-
-# These can be any stocks you'd like,
-# but for testing purposes, I've
-# picked these five.
-
-stocks_dictionary = {
-    'TSLA': 0,
-    'RTX': 1,
-    'HON': 2,
-    'KBR': 3,
-    'AAPL': 4
-}
-
-stocks_list = [TSLA, RTX, HON, KBR, AAPL]
